797cff2fd8d6072dba7a78a6af2a01ce1e862b40
Andrzej Ciombor <andrzejciombor@gmail.com>
2021-01-10 09:40:46 +0100

    Add 3

--- SomeProgram.java
+++ SomeProgram.java
@@ -3,5 +3,6 @@ gipublic class SomeProgram {
       System.out.println("We are learning to use Git."); 
 	  System.out.println("We will be Git experts, soon.");
 	  System.out.println("We will be Git experts, soon.");
+	   System.out.println("We will be Git experts, soon. 3");
     }
 }
